0e6aecf123da1e2844b017d2c5894886676f2b01
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2026-04-25 19:27:24 -0400

    feat: add token & tokentype

---
+++ lox/token.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type TokenType int
+
+const (
+	LEFT_PAREN TokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+	IDENTIFIER
+	STRING
+	NUMBER
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+	EOF
+)
+
+type Token struct {
+	tokenType  TokenType
+	lexeme     string
+	lineNumber int
+	literal interface{}
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("%v %v %v", t.tokenType, t.lexeme, t.literal)
+}
+
+func NewToken(tokenType TokenType, lexeme string, lineNumber int, literal interface{}) Token {
+	newToken := Token{tokenType: tokenType, lexeme: lexeme, lineNumber: lineNumber, literal: literal}
+	return newToken
+}
