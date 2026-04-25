b52c25b023ff1a7993e2e15bc03c96cdae7f0e5a
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2026-04-25 18:42:34 -0400

    feat: add error handling to Interpreter struct

--- lox/interpreter.go
+++ lox/interpreter.go
@@ -3,36 +3,50 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 )
 
-type Interpreter struct{
+type Interpreter struct {
 	hadError bool
 }
 
 func (i *Interpreter) runFile(arg string) {
 	bytes, err := os.ReadFile(arg)
 	if err != nil {
-		log.Fatal("[Error] Unable to read file.")
+		fmt.Fprintln(os.Stderr, "[Error] Unable to read file.")
+		os.Exit(66)
 	}
 	i.run(string(bytes))
+	if i.hadError {
+		os.Exit(65)
+	}
 }
 
 func (i *Interpreter) runPrompt() {
 	reader := bufio.NewReader(os.Stdin)
-	log.Println("Interactive mode. Enter Lox code below. Ctrl+D to exit.")
+	fmt.Println("Interactive mode. Enter Lox code below. Ctrl+D to exit.")
 	for {
 		fmt.Print("> ")
 		line, err := reader.ReadString('\n')
 		if err != nil {
+			fmt.Println("\nExiting interactive mode.")
 			break
 		}
 		i.run(strings.TrimSpace(line))
+		i.hadError = false
 	}
 }
 
 func (i *Interpreter) run(source string) {
 	fmt.Println(source)
+}
+
+func (i *Interpreter) error(line int, message string) {
+	i.report(line, "", message)
+}
+
+func (i *Interpreter) report(line int, where, message string) {
+	fmt.Fprintf(os.Stderr, "[line %d] Error%s: %s\n", line, where, message)
+	i.hadError = true
 }
--- lox/main.go
+++ lox/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 )
 
 func main() {
-	log.Println("Go-Lox interpreter started.")
+	fmt.Println("Go-Lox interpreter started.")
 
 	args := os.Args
 	interpreter := &Interpreter{}
-	
+
 	if len(args) > 2 {
-		log.Fatal("[Error] Correct usage: 'go run main.go [script_name].'")
+		fmt.Fprintln(os.Stderr, "[Error] Correct usage: 'go run . [script_name].'")
+		os.Exit(64)
 	} else if len(args) == 2 {
 		interpreter.runFile(args[1])
 	} else {
