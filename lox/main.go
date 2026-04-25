0bb3961444ca871d2d32dba4041a79f578dcc150
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2026-04-25 17:25:24 -0400

    feat: add file and REPL execution modes

--- lox/main.go
+++ lox/main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, Lox!")
+	log.Println("Go-Lox interpreter started.")
+	args := os.Args
+	if len(args) > 2 {
+		log.Fatal("[Error] Correct usage: 'go run main.go [script_name].'")
+	} else if len(args) == 2 {
+		runFile(args[1])
+	} else {
+		runPrompt()
+	}
 }
-i
+
+func runFile(arg string) {
+	bytes, err := os.ReadFile(arg)
+	if err != nil {
+		log.Fatal("[Error] Unable to read file.")
+	}
+	run(string(bytes))
+}
+
+func runPrompt() {
+	reader := bufio.NewReader(os.Stdin)
+	log.Println("Interactive mode. Enter Lox code below. Ctrl+D to exit.")
+	for {
+		fmt.Print("> ")
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		run(strings.TrimSpace(line))
+	}
+}
+
+func run(source string) {
+	fmt.Println(source)
+}
