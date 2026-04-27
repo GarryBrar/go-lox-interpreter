fefdbb37683eac6010e2814f9c2ba4cd1fdc7724
Garry Brar <76575127+GarrySinghBr@users.noreply.github.com>
2026-04-27 00:08:46 -0400

    feat: add operator support to scanner

--- lox/scanner.go
+++ lox/scanner.go
@@ -30,29 +30,53 @@ TODO: REFACTOR LATER
 func (s *Scanner) scanToken() {
 	char := s.advance()
 	switch char {
-    case '(':
-        s.addToken(LEFT_PAREN)
-    case ')':
-        s.addToken(RIGHT_PAREN)
-    case '{':
-        s.addToken(LEFT_BRACE)
-    case '}':
-        s.addToken(RIGHT_BRACE)
-    case ',':
-        s.addToken(COMMA)
-    case '.':
-        s.addToken(DOT)
-    case '-':
-        s.addToken(MINUS)
-    case '+':
-        s.addToken(PLUS)
-    case ';':
-        s.addToken(SEMICOLON)
-    case '*':
-        s.addToken(STAR)
-    default:
-        s.reporter.error(s.line, "Unexpected character.")
-    }
+	case '(':
+		s.addToken(LEFT_PAREN)
+	case ')':
+		s.addToken(RIGHT_PAREN)
+	case '{':
+		s.addToken(LEFT_BRACE)
+	case '}':
+		s.addToken(RIGHT_BRACE)
+	case ',':
+		s.addToken(COMMA)
+	case '.':
+		s.addToken(DOT)
+	case '-':
+		s.addToken(MINUS)
+	case '+':
+		s.addToken(PLUS)
+	case ';':
+		s.addToken(SEMICOLON)
+	case '*':
+		s.addToken(STAR)
+	case '!':
+		if s.match('=') {
+			s.addToken(BANG_EQUAL)
+		} else {
+			s.addToken(BANG)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EQUAL_EQUAL)
+		} else {
+			s.addToken(EQUAL)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LESS_EQUAL)
+		} else {
+			s.addToken(LESS)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GREATER_EQUAL)
+		} else {
+			s.addToken(GREATER)
+		}
+	default:
+		s.reporter.error(s.line, "Unexpected character.")
+	}
 }
 
 func (s *Scanner) isAtEnd() bool {
@@ -60,16 +84,27 @@ func (s *Scanner) isAtEnd() bool {
 }
 
 func (s *Scanner) advance() byte {
-    c := s.source[s.current]
-    s.current++
-    return c
+	c := s.source[s.current]
+	s.current++
+	return c
 }
 
-func (s* Scanner) addToken(tokenType TokenType){
+func (s *Scanner) addToken(tokenType TokenType) {
 	s.addTokenLiteral(tokenType, nil)
 }
 
 func (s *Scanner) addTokenLiteral(tokenType TokenType, literal interface{}) {
 	text := s.source[s.start:s.current]
 	s.tokens = append(s.tokens, NewToken(tokenType, text, s.line, literal))
+}
+
+func (s *Scanner) match(expected byte) bool {
+	if isAtEnd() {
+		return false
+	}
+	if s.source[s.current] != expected {
+		return false
+	}
+	s.current := s.current + 1
+	return true
 }
